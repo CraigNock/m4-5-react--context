e2a1a2f7255c2b0ec1dca72aa626e6feeb829f89
Craig Nockels <cnockels@gmail.com>
2020-03-12 16:44:56 -0400

    qol update

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,7 +3,7 @@ const items = [
     { type:'passive', id: 'cursor', name: 'Cursor', cost: 10, value: 1 },
     { type:'passive', id: 'grandma', name: 'Grandma', cost: 100, value: 10 },
     { type:'passive', id: 'farm', name: 'Farm', cost: 1000, value: 80 },
-    { type:'active', id: 'double', name: 'Double Cursor', cost: 50, value: 1 }
+    { type:'active', id: 'double', name: 'Double Click', cost: 50, value: 1 }
 ];
 
 export default items;
--- src/hooks/useKeydown.js
+++ src/hooks/useKeydown.js
@@ -4,6 +4,8 @@ import React from 'react';
 const useKeydown = (key, keyFunction) => {
   React.useEffect(() => {
     const handleKeydown = ev => {
+      ev.preventDefault();
+      if (ev.repeat) { return };
       if (ev.code === key) {
         keyFunction();
       }
